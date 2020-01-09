ac8175776c1cbe9a58e808b10ef17306f4e52832
mik <autoclik974@gmail.com>
2020-01-09 17:07:59 +0100

    first commit

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -2,7 +2,7 @@ require 'facebook/messenger'
 include Facebook::Messenger
 
 Facebook::Messenger::Subscriptions.subscribe(
-  access_token: access_token
+  access_token: ENV['access_token']
 )
 
 Bot.on :message do |message|
